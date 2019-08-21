528d86ece353022dc64ad5a3eb9ad1cd1489e47b
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-21 16:46:29 +0700

    add_users#new,#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   before_action :load_user, only: %i|edit update|
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def index
     @users = User.all
   end
