cb6a3917d98e2d5a24372bd930b2cfcbc9427009
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-21 16:43:17 +0700

    add_users#edit,#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: %i|edit update|
+
   def index
     @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit :full_name
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :full_name, presence: true
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def full_name=(name)
+    parts = name.split(" ", 2)
+    self.first_name = parts[0]
+    self.last_name = parts[1]
   end
 end
